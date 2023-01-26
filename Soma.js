0393b32899643689a7cab803007ed3b7b7c03396
Bruno Lima <brunopinheirolima9@gmail.com>
2023-01-25 22:50:50 -0400

    codigo de soma utilizando função

---
+++ Soma.js
@@ -0,0 +1,9 @@
+function soma(n1,n2){ //Aqui ei abri uma função
+    var n1 = Number(prompt("digite seu o primeiro numero")) // digite o primeiro numero 
+    var n2 = Number(prompt("digite o segundo numero")) //digite o segundo numero
+
+    var res =  (n1 + n2) //criei uma variavel res de para pegar o resulado da soma
+    return n1 + n2 //retornar o valor
+}
+
+alert(soma()) //imprimir o valor obtido
